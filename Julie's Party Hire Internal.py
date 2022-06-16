b8d8bddfc2b7a5c55b3178222e7631db05a4f3d3
Ved <st21120@ormiston.school.nz>
2022-06-17 09:32:34 +1200

    Added Buttons

--- Julie's Party Hire Internal.py
+++ Julie's Party Hire Internal.py
@@ -6,22 +6,43 @@
 from tkinter import*
 from tkinter import ttk
 
-root=Tk()
+main_window=Tk()
 
 #Create quit subroutine
 def quit():
     main_window.destroy()
 
 
-#Create labels
-lbltitle = ttk.Label(root, text="Julies Party Hire", font=("Helvetica 30 bold"))
-lblfirstname = ttk.Label(root, text="First Name: ", font=("Helvetica 20"))
-lbllastname = ttk.Label(root, text="Last Name: ", font=("Helvetica 20"))
+#Create and grid labels
+lbltitle = ttk.Label(main_window, text="Julies Party Hire", font=("Helvetica 30 bold"))
+lblfirstname = ttk.Label(main_window, text="First Name: ", font=("Helvetica 20"))
+lbllastname = ttk.Label(main_window, text="Last Name: ", font=("Helvetica 20"))
+lblreceiptnumber = ttk.Label(main_window, text="Receipt Number: ", font=("Helvetica 20"))
+lblitemhired = ttk.Label(main_window, text="Items Hired: ", font=("Helvetica 20"))
+
+lbltitle.grid(row=0, column=1, columnspan=1)
+lblfirstname.grid(row=1, column=0, columnspan=1, sticky = W)
+lbllastname.grid(row=2, column=0, columnspan=1, sticky = W)
+lblreceiptnumber.grid(row=3, column=0, columnspan=1, sticky = W)
+lblitemhired.grid(row=4, column=0, columnspan=1, sticky = W)
+
+#buttons
+def buttons():
+    Button(main_window, text="Update").grid(column=5,row=1)
+    Button(main_window, text="Print").grid(column=2,row=1)
+    Button(main_window, text="Quit",command=quit) .grid(column=3,row=1)
+    Button(main_window, text="Delete").grid(column=4,row=6)
 
 
-lbltitle.grid(row=0, column=1, columnspan=2)
-lblfirstname.grid(row=1, column=0, columnspan=2)
-lbllastname.grid(row=2, column=0, columnspan=2)
 
-root.geometry("500x450")
-root.mainloop()
+
+lbltitle.grid(row=0, column=1, columnspan=1)
+lblfirstname.grid(row=1, column=0, columnspan=1, sticky = W)
+lbllastname.grid(row=2, column=0, columnspan=1, sticky = W)
+lblreceiptnumber.grid(row=3, column=0, columnspan=1, sticky = W)
+lblitemhired.grid(row=4, column=0, columnspan=1, sticky = W)
+
+
+main_window.geometry("500x450")
+buttons()
+main_window.mainloop()
