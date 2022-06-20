f75b92abe13e9ed40a3faafea9ceeeb1692db3d7
Ved <st21120@ormiston.school.nz>
2022-06-21 11:33:24 +1200

    Entry Boxes added

--- Julie's Party Hire Internal.py
+++ Julie's Party Hire Internal.py
@@ -3,6 +3,7 @@
 #Date: 10 June 2022
 
 #Import tkinter
+from re import A
 from tkinter import*
 from tkinter import ttk
 
@@ -12,6 +13,17 @@ main_window=Tk()
 def quit():
     main_window.destroy()
 
+#Create append subroutine
+def append():
+    global hire_details, entry, entry1, entryIH, entry_quantity
+    # append each item to its own area of the list
+    customer_details.append([entry.get(), entry1.get(), entryIH.get(), entry_quantity.get()])
+    #clear the boxes
+    entry.delete(0, 'end')
+    entry1.delete(0, 'end')
+    entryIH.delete(0, 'end')
+    entry_quantity.delete(0, 'end')
+    total_entries += 1
 
 #Create and grid labels
 lbltitle = ttk.Label(main_window, text="Julies Party Hire", font=("Helvetica 20 bold"))
@@ -28,14 +40,29 @@ lblitemhired.grid(row=4, column=0, columnspan=1, sticky = W)
 
 #Create Buttons
 def buttons():
-    Button(main_window, text="Update", font=("Helvetica 12")).grid(column=4,row=1)
+    Button(main_window, text="Append", font=("Helvetica 12")).grid(column=4,row=1)
     Button(main_window, text="Print", font=("Helvetica 12")).grid(column=2,row=1)
     Button(main_window, text="Quit", font=("Helvetica 12"), command=quit) .grid(column=3,row=1)
     Button(main_window, text="Delete", font=("Helvetica 12")).grid(column=3,row=4)
 
 #Create Entry Boxes
+
+hire_details = []
 entry_firstname = Entry(main_window, font="Helvitica 12")
 entry_firstname.grid(column=1,row=1, sticky = W)
+
+hire_details = []
+entry_lastname = Entry(main_window, font="Helvitica 12")
+entry_lastname.grid(column=1,row=2, sticky = W)
+
+hire_details = []
+entry_receiptnumber = Entry(main_window, font="Helvitica 12")
+entry_receiptnumber.grid(column=1,row=3, sticky = W)
+
+hire_details = []
+entry_itemhired = Entry(main_window, font="Helvitica 12")
+entry_itemhired.grid(column=1,row=4, sticky = W)
+
 
 
 main_window.geometry("500x450")
